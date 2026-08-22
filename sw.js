58a2fdf3242806b938d9a494c12bab6a8d057c05
Aswin <aswin@macbookpro.lan>
2026-08-22 10:25:37 +0530

    Remove mobile tap highlight flash

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "luma-clock-v9";
+const CACHE = "luma-clock-v10";
 const APP = ["./", "./index.html", "./styles.css", "./app.js?v=9", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
